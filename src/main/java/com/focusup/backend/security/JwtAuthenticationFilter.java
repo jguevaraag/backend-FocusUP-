5c32a3cca1727abc7a299a4b35d80542c474507a
jguevaraag <javitmdfe@gmail.com>
2026-05-10 13:07:54 +0200

    Pequeño arreglo para error 500 del Frontend

--- src/main/java/com/focusup/backend/security/JwtAuthenticationFilter.java
+++ src/main/java/com/focusup/backend/security/JwtAuthenticationFilter.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService = new JwtService();
+    private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
     @Override
